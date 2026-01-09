f8025f30bdc1479165504dcf3c1773b7a7e057c9
Yan Xia <isshikihugh@gmail.com>
2026-01-09 12:54:55 -0600

    update: edb

--- ez4d/debug/__init__.py
+++ ez4d/debug/__init__.py
@@ -4,6 +4,12 @@ Debug utilities for fast debugging mainly in CLI situations.
 import ez4d.debug as edb
 """
 
+from ipdb import set_trace
+bp = set_trace  # shortcut for set_trace
+
+from rich import inspect
+
+
 from .stack_utils import show_call_stacks as trace_up
 
-__all__ = ['trace_up']
+__all__ = ['set_trace', 'bp', 'inspect', 'trace_up']
