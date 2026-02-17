cdae3beb9211b430cbd90da4064536fd5b034f15
Yan Xia <isshikihugh@gmail.com>
2026-02-17 11:33:11 -0600

    update: evaluator

--- ez4d/eval/base_evaluator.py
+++ ez4d/eval/base_evaluator.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 class EvaluatorBase():
     ''' To use this class, you should inherit it and implement the `eval` method. '''
     def __init__(self):
-        self.accumulator = defaultdict(list)
+        self.aggregator = defaultdict(list)
 
     def eval(self, **kwargs):
         ''' Evaluate the metrics on the data. '''
@@ -14,18 +14,18 @@ class EvaluatorBase():
     def get_results(self, chosen_metric=None):
         ''' Get the current mean results. '''
         # Only chosen metrics will be compacted and returned.
-        compacted = self._compact_accumulator(chosen_metric)
+        compacted = self._compact_aggregator(chosen_metric)
         ret = {}
         for k, v in compacted.items():
             ret[k] = v.mean(dim=0).item()
         return ret
 
-    def _compact_accumulator(self, chosen_metric=None):
-        ''' Compact the accumulator list and return the compacted results. '''
+    def _compact_aggregator(self, chosen_metric=None):
+        ''' Compact the aggregator list and return the compacted results. '''
         ret = {}
-        for k, v in self.accumulator.items():
+        for k, v in self.aggregator.items():
             # Only chosen metrics will be compacted.
             if chosen_metric is None or k in chosen_metric:
                 ret[k] = torch.cat(v, dim=0)
-                self.accumulator[k] = [ret[k]]
+                self.aggregator[k] = [ret[k]]
         return ret
